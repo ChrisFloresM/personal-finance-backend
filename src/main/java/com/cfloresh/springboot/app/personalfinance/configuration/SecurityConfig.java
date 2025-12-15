6318bdafd94c1cb9398147c38996480f5b9e605c
Christian <christian.mig.flmo@gmail.com>
2025-12-15 11:48:20 -0600

    Setup of the project

---
+++ src/main/java/com/cfloresh/springboot/app/personalfinance/configuration/SecurityConfig.java
@@ -0,0 +1,7 @@
+package com.cfloresh.springboot.app.personalfinance.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+}
